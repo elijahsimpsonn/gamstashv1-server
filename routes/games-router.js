fcb098b26e8db94899951d594bd4e34d5b0b3345
Elijah Simpson <elijahsimpson1991@gmail.com>
2020-12-17 23:30:36 -0500

    commit

---
+++ routes/games-router.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const gamesRouter = express.Router();
+const db = require("../db");
+
+gamesRouter.route("/").get(async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM games");
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        games: results.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = gamesRouter;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const consolesRouter = require('./routes/consoles-router')
-// const gamesRouter = require('./routes/games-router')
+const gamesRouter = require('./routes/games-router')
 
 const app = express()
 
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/v1/consoles', consolesRouter)
-// app.use('/api/v1/games', gamesRouter)
+app.use('/api/v1/games', gamesRouter)
 
 const port = process.env.PORT || 8000
 app.listen(port, () => {
