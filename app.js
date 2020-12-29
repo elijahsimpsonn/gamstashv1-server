e52db233adcc685380898ebf629041aa08111c04
Elijah Simpson <elijahsimpson1991@gmail.com>
2020-12-29 18:27:46 -0500

    changes for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require("helmet");
 
 const { NODE_ENV } = require("./config");
@@ -14,7 +15,9 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}));
 
 app.use("/api/v1/consoles", consolesRouter);
 app.use("/api/v1/games", gamesRouter);
